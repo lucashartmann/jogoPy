0e52d89ca284f0e14b367b8f7f512f359a642f48
lucashartmann <lucas.a111@hotmail.com>
2025-10-10 12:12:46 -0300

    +-

--- Emoji/Main.py
+++
@@ -1,6 +0,0 @@
-from view.App import Jogo
-
-# pyinstaller --add-data "Emoji/view;view" Emoji/Main.py
-
-if __name__ == "__main__":
-    Jogo().run()
--- Emoji/README.md
+++
@@ -1,5 +0,0 @@
-# jogoPy
-
-# Todo:
-
-1. Depois que o jogador excolhe jogar novamente depois de morrer, o personagem não consegue se mover mais
